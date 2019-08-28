de23decd18937e7f6349834e15472f77eb6f39e7
aivanova5 <ivanova.alyona5@gmail.com>
2019-08-28 16:50:21 -0700

    Updating main.sh with folders

--- main.sh
+++ main.sh
@@ -16,6 +16,10 @@ if [ ! -d "output/voltdump" ]
 then
     mkdir output/voltdump
 fi
+if [ ! -d "input/tariff" ] 
+then
+    mkdir output/tariff
+fi
 
 python3 main.py
 python3 output/billdump/bill_output.py
